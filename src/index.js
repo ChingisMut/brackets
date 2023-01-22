e8e6a6e8164d9b0362c0e71ee9f63f762b82f026
ChingisMut <mchp94@mail.ru>
2023-01-22 18:32:13 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let resPop = [];
+  let arr = str.split('');
+  if (arr.length % 2 !== 0) {
+    return false} else {
+      const arrBrackets = bracketsConfig.flat();
+      for(let i = 0; i < arr.length; i++) {
+        for(let k = 0; k < arrBrackets.length; k++) {
+          if(arr[i] === arrBrackets[k]) {
+            if(k % 2 == 0) {
+              result.push(arr[i]);
+            } else {
+              let resNew = result.concat(resPop).pop();
+              if(resNew === '(' || resNew === '[' || resNew === '{' || resNew === '|' || resNew === '1' || resNew === '3' || resNew === '5' || resNew === '7') {
+                if(arrBrackets[arrBrackets.indexOf(resNew) + 1] === arr[i]) {
+                  result.pop();
+                }
+              } else {
+                return false;
+              }
+            }
+          }
+      }
+    }
+    if(result.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
